d7b6c052da1e326d3f2308e96502c2c6eabe0177
Mohammed El Harchi <mel-harc@e2r13p3.1337.ma>
2023-06-04 22:32:38 +0100

    norm is fixed

--- libft/getstr.c
+++ libft/getstr.c
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   getstr.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-harc <mel-harc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/04 22:28:01 by mel-harc          #+#    #+#             */
+/*   Updated: 2023/06/04 22:32:27 by mel-harc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
-char *getstr(char *src)
+char	*getstr(char *src)
 {
-    char    *dst;
-    int     i;
+	char	*dst;
+	int		i;
 
-    dst = (char *)malloc((ft_strlen(src) + 1) * sizeof(char));
-    if (!dst)
-        return (NULL);
-    i = -1;
-    while(src[++i])
-        dst[i] = src[i];
-    dst[i] = '\0';
-    return (dst); 
+	dst = (char *)malloc((ft_strlen(src) + 1) * sizeof(char));
+	if (!dst)
+		return (NULL);
+	i = -1;
+	while (src[++i])
+		dst[i] = src[i];
+	dst[i] = '\0';
+	return (dst);
 }
